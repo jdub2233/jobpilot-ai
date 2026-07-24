27717f7d22609347abc721e3cd8fadde87b81be3
jdub2233 <wileyjdub@gmail.com>
2026-07-24 16:17:13 -0500

    Configure Entity Framework Core and PostgreSQL data layer

---
+++ src/JobPilotAI.Api/Data/JobPilotDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace JobPilotAI.Api.Data;
+
+public class JobPilotDbContext : DbContext
+{
+    public JobPilotDbContext(DbContextOptions<JobPilotDbContext> options)
+        : base(options)
+    {
+    }
+}
--- src/JobPilotAI.Api/Program.cs
+++ src/JobPilotAI.Api/Program.cs
@@ -1,4 +1,15 @@
+using JobPilotAI.Api.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+var connectionString =
+    builder.Configuration.GetConnectionString("JobPilotDatabase")
+    ?? throw new InvalidOperationException(
+        "Connection string 'JobPilotDatabase' was not found.");
+
+builder.Services.AddDbContext<JobPilotDbContext>(options =>
+    options.UseNpgsql(connectionString));
 
 builder.Services.AddControllers();
 
